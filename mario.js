dc9f6cf6dead5beb9de6d9b381a4e0132fa3f6f3
Jase Miller <jasemiller45@gmail.com>
2021-02-05 16:32:12 +0000

    HTML Pyramid

--- mario.js
+++ mario.js
@@ -6,15 +6,22 @@ function createHalfPyramid (height) {
     var row = '';
     
     for (var j = 1; j <= (height - i); j++) {
-      row += ' ';
+      row += '.';
     }
     
     for (var k = 1; k <= i; k++) {
-      row += '*';
+      row += '#';
     }
     
     console.log(row);
+      let para = document.createElement("p");
+          console.log(para);
+          let node = document.createTextNode(row);
+          console.log(node);
+          para.appendChild(node);
+          let element = document.getElementById("pyramid");
+          console.log(document.getElementById("pyramid"));
+          element.appendChild(para);
   }
 }
-
 createHalfPyramid(5);
